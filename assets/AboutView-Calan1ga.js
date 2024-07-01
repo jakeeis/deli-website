f8b54d66c46a22eedfbb50f6f12ed787522bbb4c
jakeeis <jacobeisenberg94@gmail.com>
2024-07-01 09:23:09 -0600

    Updates

---
+++ assets/AboutView-Calan1ga.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-Du1e4WmI.js";const t={};function n(r,a){return o(),c("h2",null,"About Page")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/ContactView-DEpemOVm.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-Du1e4WmI.js";const n={};function o(a,r){return t(),c("h2",null,"Contact Page")}const _=e(n,[["render",o]]);export{_ as default};
